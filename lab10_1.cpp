62a21389058746ab67259bc7108ad120edd8a350
Purich Seenuallae <purichseenuallae@gmail.com>
2021-01-12 16:51:43 +0700

    finished

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -1,2 +1,20 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+#include <iomanip>
+#include <cmath>
+#include <fstream>
+
+using namespace std;
+int main()
+{
+    srand(time(0));
+    cout << "Press Enter 3 times to reveal your future.";
+    cin.get();
+    cin.get();
+    cin.get();
+    string g[] = {"A", "B+", "B", "C+", "C", "D+", "D", "F", "W"};
+    cout << "You will get " << g[rand() % 9] << " in this 261102.";
+}
+
